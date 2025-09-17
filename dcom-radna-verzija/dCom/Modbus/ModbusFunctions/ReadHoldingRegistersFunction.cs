f863c0c3ee18f4e8f2178b1bc61b19c9526a7a53
Dajana Bulic <bulic.dajanaaa@gmail.com>
2025-09-17 02:49:17 +0200

    modbus zavrsen

--- dcom-radna-verzija/dCom/Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
+++ dcom-radna-verzija/dCom/Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
@@ -24,15 +24,65 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusCommandParameters paramCom = this.CommandParameters as ModbusCommandParameters;
+            byte[] request = new byte[12];
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(
+                    IPAddress.HostToNetworkOrder(
+                        (short)paramCom.TransactionId)),
+                0,
+                (Array)request,
+                0,
+                2);
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(
+                    IPAddress.HostToNetworkOrder(
+                        (short)paramCom.ProtocolId)),
+                0,
+                (Array)request,
+                2,
+                2);
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(
+                    IPAddress.HostToNetworkOrder(
+                        (short)paramCom.Length)),
+                0,
+                (Array)request,
+                4,
+                2);
+            request[6] = paramCom.UnitId;
+            request[7] = paramCom.FunctionCode;
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(
+                    IPAddress.HostToNetworkOrder(
+                        (short)(paramCom as ModbusReadCommandParameters).StartAddress)),
+                0,
+                (Array)request,
+                8,
+                2);
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(
+                    IPAddress.HostToNetworkOrder(
+                        (short)(paramCom as ModbusReadCommandParameters).Quantity)),
+                0,
+                (Array)request,
+                10,
+                2);
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters paramCom = this.CommandParameters as ModbusReadCommandParameters;
+            Dictionary<Tuple<PointType, ushort>, ushort> d = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            ushort startAddress = paramCom.StartAddress;
+            byte byteCount = response[8];
+            ushort value;
+
+            for (int i = 0; i < byteCount; i += 2)
+            {
+                Tuple<PointType, ushort> address = new Tuple<PointType, ushort>(PointType.ANALOG_OUTPUT, startAddress++);
+                value = (ushort)IPAddress.NetworkToHostOrder(BitConverter.ToInt16(response, 9 + i));
+                d.Add(address, value);
+            }
+            return d;
         }
     }
 }
--- dcom-radna-verzija/dCom/Modbus/ModbusFunctions/ReadInputRegistersFunction.cs
+++ dcom-radna-verzija/dCom/Modbus/ModbusFunctions/ReadInputRegistersFunction.cs
@@ -24,14 +24,66 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
+            ModbusCommandParameters paramCom = this.CommandParameters as ModbusCommandParameters;
+            byte[] request = new byte[12];
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(
+                    IPAddress.HostToNetworkOrder(
+                        (short)paramCom.TransactionId)),
+                0,
+                (Array)request,
+                0,
+                2);
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(
+                    IPAddress.HostToNetworkOrder(
+                        (short)paramCom.ProtocolId)),
+                0,
+                (Array)request,
+                2,
+                2);
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(
+                    IPAddress.HostToNetworkOrder(
+                        (short)paramCom.Length)),
+                0,
+                (Array)request,
+                4,
+                2);
+            request[6] = paramCom.UnitId;
+            request[7] = paramCom.FunctionCode;
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(
+                    IPAddress.HostToNetworkOrder(
+                        (short)(paramCom as ModbusReadCommandParameters).StartAddress)),
+                0,
+                (Array)request,
+                8,
+                2);
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(
+                    IPAddress.HostToNetworkOrder(
+                        (short)(paramCom as ModbusReadCommandParameters).Quantity)),
+            0,
+                            (Array)request,
+                10,
+                2);
+            return request;
             throw new NotImplementedException();
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
+            ModbusReadCommandParameters paramCom = this.CommandParameters as ModbusReadCommandParameters;
+            Dictionary<Tuple<PointType, ushort>, ushort> d = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            ushort address = paramCom.StartAddress;
+            for (int i = 0; i < response[8] / 2; i++)
+            {
+                byte byte1 = response[9 + i * 2];
+                byte byte2 = response[10 + i * 2];
+                ushort value1 = BitConverter.ToUInt16(new byte[2] { byte2, byte1 }, 0);
+
+                d.Add(new Tuple<PointType, ushort>(PointType.ANALOG_INPUT, address), value1);
+                address++;
+            }
+            return d;
             throw new NotImplementedException();
         }
     }
