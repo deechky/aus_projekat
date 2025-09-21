aaab6ed6b32361e9c2a5cd16d814a3a352b4f27e
caparaivan <capara.ivan359@gmail.com>
2025-09-21 15:33:31 +0200

    ReadCoilsFunction

--- dcom-radna-verzija/dCom/Modbus/ModbusFunctions/ReadCoilsFunction.cs
+++ dcom-radna-verzija/dCom/Modbus/ModbusFunctions/ReadCoilsFunction.cs
@@ -25,14 +25,40 @@ namespace Modbus.ModbusFunctions
         public override byte[] PackRequest()
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            // kreira niz bajtova za sve parametre za modbus
+            var p = (ModbusReadCommandParameters)CommandParameters;
+            byte[] request = new byte[12];
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)p.TransactionId)), 0, request, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)p.ProtocolId)), 0, request, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)p.Length)), 0, request, 4, 2);
+            request[6] = p.UnitId;
+            request[7] = p.FunctionCode;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)p.StartAddress)), 0, request, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)p.Quantity)), 0, request, 10, 2);
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            // provjerava koji coil je ON a koji off
+            var p = (ModbusReadCommandParameters)CommandParameters;
+            var result = new Dictionary<Tuple<PointType, ushort>, ushort>();
+            byte byteCount = response[8];
+            ushort addr = p.StartAddress;
+
+            for (int i = 0; i < byteCount; i++)
+            {
+                byte b = response[9 + i];
+                for (int bit = 0; bit < 8 && addr < p.StartAddress + p.Quantity; bit++)
+                {
+                    ushort val = (ushort)((b >> bit) & 1);
+                    result.Add(Tuple.Create(PointType.DIGITAL_OUTPUT, addr++), val);
+                }
+            }
+            return result;
         }
     }
 }
